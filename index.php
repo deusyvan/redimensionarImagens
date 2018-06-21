c70514fd609b21ae7172c6f3a5d17d6d5c42104a
deusyvan <deusyvan@gmail.com>
2018-06-20 21:36:54 -0300

    Começando estudo de colocação de marca dágua

--- index.php
+++ index.php
@@ -1,28 +1,5 @@
 <?php
 
-    $arquivo = "wallpaper-1210239.jpg";
     
-    $largura = 200;
-    $altura = 200;
-    
-    list($largura_original, $altura_original) = getimagesize($arquivo);
-    
-    $ratio = $largura_original / $altura_original;
-    
-    if($largura/$altura > $ratio){
-        $largura = $altura * $ratio;
-    } else {
-        $altura = $largura / $ratio;
-    }
-    
-    $imagem_final = imagecreatetruecolor($largura, $altura);
-    $imagem_original = imagecreatefromjpeg($arquivo);
-    
-    imagecopyresampled($imagem_final, $imagem_original, 
-        0, 0, 0, 0 , $largura, $altura, $largura_original, $altura_original);
-    
-    imagejpeg($imagem_final, "mini_imagem.jpeg", 100);
-    
-    echo "Imagem redimencionada com sucesso!"
     
 ?>
