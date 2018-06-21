d6aec3d001330f1bede38e23e6d2aeb95003d9e3
deusyvan <deusyvan@gmail.com>
2018-06-20 21:43:35 -0300

    Criamos uma nova imagem e pegamos as imagens grande e miniatura

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
+    $imagem = "wallpaper-1210239.jpg";
+    
+    list($largura_original, $altura_original) = getimagesize($imagem);
+    
+    $imagem_final = imagecreatetruecolor($largura_original, $altura_original);
+    
+    $imagem_original = imagecreatefromjpeg("wallpaper-1210239.jpg");
+    $imagem_mini = imagecreatefromjpeg("mini_imagem.jpeg");
     
     
 ?>
